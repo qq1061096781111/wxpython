9db35a20f7a0eda3af10050112b8db908c23ad38
lifengping <lifengping@live.cn>
2021-01-30 16:42:23 +0800

    first

---
+++ index.wsgi
@@ -0,0 +1,18 @@
+# _*_ coding: utf-8 _*_
+
+import os
+import sae
+import web
+from weixin import WeixinInterface
+
+url = (
+'/weixin','WeixinInterface'
+
+)
+
+app_root = os.path.dirname(__file__)
+templates_root = os.path.join(self.app_root,'templates')
+render = web.template.render(self.templates_root)
+
+app = web.application(urls,globals()).wsgifunc()
+application = sae.create_wsgi_app(app)
---
+++ weixin.py
@@ -0,0 +1,27 @@
+# _*_ coding: utf-8 _*_
+import web
+import os
+import  hashlib
+import time
+class  WeixinInterface:
+    def __init__(self):
+        self.app_root = os.path.dirname(__file__)
+        self.templates_root = os.path.join(self.app_root,'templates')
+        self.render = web.template.render(self.templates_root)
+
+    def GET(self):
+        data = web.input()
+        signature = data.signature
+        timestamp = data.timestamp
+        nonce = data.nonce
+        echostr = data.echostr
+        token = "wxpython"
+
+        l = [token,timestamp,nonce]
+        l.sort()
+        sha1 = hashlib.sha1()
+        map(sha1.update,l)
+        hashcode = sha1.hexdigest()
+
+        if hashcode == signature:
+            return echostr
